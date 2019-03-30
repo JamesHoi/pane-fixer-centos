a6449b580806cc48c25f78f15dfa00e7edc41a59
JamesHoi <james352053767@gmail.com>
2019-03-31 01:52:11 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# pane-fixer-centos
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#install font for software
+yum install kde-l10n-Chinese
+yum install chkfontpath-2.0.3-alt1.x86_64.rpm
+yum install fonts-ISO8859-2-75dpi-1.0-17.1.noarch.rpm
+yum install fonts-chinese-3.02-12.el5.noarch.rpm
